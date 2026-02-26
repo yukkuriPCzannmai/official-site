a33eb98b044b3497fadeacd8c1d73944a75bb8bc
yukkuriPCzannmai <basukettomannnanoda@gmail.com>
2026-02-26 19:42:52 +0900

    Create youtube.js

---
+++ functions/api/youtube.js
@@ -0,0 +1,24 @@
+export async function onRequest(context) {
+  const { searchParams } = new URL(context.request.url);
+  const action = searchParams.get('action');
+  const channelId = searchParams.get('channelId');
+  const playlistId = searchParams.get('playlistId');
+  const pageToken = searchParams.get('pageToken') || '';
+  
+  // ここに自分のYouTube APIキーを入れる
+  const API_KEY = "context.env.YOUTUBE_API_KEY";
+
+  let url = "";
+  if (action === 'channel') {
+    url = `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${channelId}&key=${API_KEY}`;
+  } else if (action === 'playlist') {
+    url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,resourceId&playlistId=${playlistId}&maxResults=50&pageToken=${pageToken}&key=${API_KEY}`;
+  }
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
