c31d9605791af81ca8c8e4bdf1da3b352739e937
yukkuriPCzannmai <basukettomannnanoda@gmail.com>
2026-02-28 22:15:17 +0900

    Update youtube.js

--- functions/api/youtube.js
+++ functions/api/youtube.js
@@ -5,14 +5,13 @@ export async function onRequest(context) {
   const playlistId = searchParams.get('playlistId');
   const pageToken = searchParams.get('pageToken') || '';
   
-  // ここに自分のYouTube APIキーを入れる
   const API_KEY = context.env.YOUTUBE_API_KEY;
 
   let url = "";
   if (action === 'channel') {
     url = `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${channelId}&key=${API_KEY}`;
   } else if (action === 'playlist') {
-    url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,resourceId&playlistId=${playlistId}&maxResults=50&pageToken=${pageToken}&key=${API_KEY}`;
+    url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=50&pageToken=${pageToken}&key=${API_KEY}`;
   }
 
   const response = await fetch(url);
